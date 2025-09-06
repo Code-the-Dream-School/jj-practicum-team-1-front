37b3150ff926e2c7ac981dea469494b150c998f3
stu <stumac1000@yahoo.com>
2025-09-05 17:13:09 -0700

    Add page not found component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Navbar from "./components/shared/Navbar";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import HomePage from "./pages/HomePage";
 import PlantsPage from "./pages/PlantsPage";
 import PlantDetailPage from "./pages/PlantDetailPage";
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
   return (
@@ -17,7 +23,8 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/plants" element={<PlantsPage />} />
         <Route path="/plants/:id" element={<PlantDetailPage />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="/page-not-found" element={<PageNotFound />} />
+        <Route path="*" element={<Navigate to="/page-not-found" replace />} />
       </Routes>
     </>
   );
